5c4e8d1cc44d25a0fd3e02c969a24aba8b13b52f
stef108 <70521492+stef108@users.noreply.github.com>
2021-06-23 16:48:37 +0200

    fixed https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')) {
+        if ($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
+    }
 
 }
